24b18cbd1e1eea59f4c5e24a519d333902131fad
Mark <mjimenez@habaneroconsulting.com>
2019-01-02 05:15:53 -0500

    Added missing setting in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,3 +42,9 @@ exports.createPages = ({actions, graphql}) => {
 		})
 	})
 }
+
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+
+exports.onCreateNode = ({ node }) => {
+	fmImagesToRelative(node);
+};
