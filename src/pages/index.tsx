3a4d4b08cc27392fe86cba1896c14f5e50c0f067
Mark <mjimenez@habaneroconsulting.com>
2019-01-03 04:16:47 -0500

    Reverted typography change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from '../layouts/default'
 
 const IndexPage = () => (
 	<Layout alignment="middle">
-		<div className="introduction">
+		<div>
 			<h1>Hello!</h1>
 			<p>My name is Mark Jimenez. I am a recent graduate from George Brown College and majored in Electro-Mechanical Engineering. Although my studies involved hardware, my passion lies within -code-. Since then, I have been elevating this passion of mine onto a more professional level by self-teaching and getting involved in the web development community.</p>
 			<p>As a result of months and months of learning, I have recently obtained a Junior Front-End Developer internship at Habanero Consulting Group. On the side, I am working free-lance where I have worked with 2 clients in the past year.</p>
