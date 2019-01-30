6c8b69f20a1461bbd74525a1211a4b66725e3ddb
Mark <mjimenez@habaneroconsulting.com>
2019-01-29 20:12:58 -0500

    Fixed panel headings on very small phone sizes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import getControl from '../scripts/utilities/getControl';
 
 // Research about ref in React
 // let button: ReactNode;
-const HEADER_EXPANDED_CLASSNAME = 'aside-main__content--expand';
+const HEADER_EXPANDED_CLASSNAME = 'header--active';
 
 function onMenuClick(e) {
 	const header = getControl(e.currentTarget);
@@ -19,9 +19,9 @@ function onMenuClick(e) {
 
 const Header = ({ title, menuPages, logo }) => (
 	<>
-		<nav className="header">
+		<nav className="header" id="header">
 			<div className="header__container">
-				<button className="header__button">
+				<button aria-controls="header" className="header__button" onClick={onMenuClick}>
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="menu"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><rect x="3" y="11" width="18" height="2" rx=".95" ry=".95"/><rect x="3" y="16" width="18" height="2" rx=".95" ry=".95"/><rect x="3" y="6" width="18" height="2" rx=".95" ry=".95"/></g></g></svg>
 					<span className="u-hidden">Open navigation</span>
 				</button>
