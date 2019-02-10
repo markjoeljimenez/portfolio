4afa7d22059bde8a94c7c70da18cacf639af372d
Mark <mjimenez@habaneroconsulting.com>
2019-02-10 18:58:42 -0500

    Added sort

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,12 @@ import remarkHtml from 'remark-html';
 
 import Layout from '../layouts/default';
 
-
 const IndexPage = () => (
 	<StaticQuery
 		query={graphql`
 		query IndexQuery {
 			allFile(
-				filter: { extension: { eq: "md"} }
+				filter: { internal: { mediaType: { eq: "text/markdown" } } }
 			) {
 				edges {
 					node {
@@ -19,6 +18,7 @@ const IndexPage = () => (
 						sourceInstanceName
 						childMarkdownRemark {
 							frontmatter {
+								date
 								title
 								shortDescription
 								client
@@ -52,12 +52,6 @@ const IndexPage = () => (
 			return (
 			<>
 				<Layout>
-					{/* <div className="intro">
-						<h1>Hello!</h1>
-						<p>My name is Mark Jimenez. I am a recent graduate from George Brown College and majored in Electro-Mechanical Engineering. Although my studies involved hardware, my passion lies within -code-. Since then, I have been elevating this passion of mine onto a more professional level by self-teaching and getting involved in the web development community.</p>
-						<p>As a result of months and months of learning, I have recently obtained a Junior Front-End Developer internship at Habanero Consulting Group. On the side, I am working free-lance where I have worked with 2 clients in the past year.</p>
-						<p>I love to invent, innovate, and expand on new ideas using what I've learned in college and what I'm currently learning throughout my career as a Web Developer.</p>
-					</div> */}
 					<div className="hero-panel panel">
 						<div className="panel__container">
 							<div className="row row--no-margin-top">
@@ -70,7 +64,9 @@ const IndexPage = () => (
 						</div>
 					</div>
 
-					{data.allFile.edges.map(project => {
+					{data.allFile.edges.sort((a, b) => {
+						return new Date(b.node.childMarkdownRemark.frontmatter.date) - new Date(a.node.childMarkdownRemark.frontmatter.date);
+					}).map(project => {
 						const frontmatter = project.node.childMarkdownRemark.frontmatter;
 
 						const reverse = (frontmatter.reverse ? ' panel--reverse' : '');
