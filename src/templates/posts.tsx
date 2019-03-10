cf982d9f0a000a6ef433fcdf02baa42de872f42c
Mark Jimenez <markjimenez@Marks-MacBook-Pro.local>
2019-03-10 15:35:50 -0400

    Update blog post styling

--- src/templates/posts.tsx
+++ src/templates/posts.tsx
@@ -16,8 +16,10 @@ export default function Template({ data }) {
 						<div className="row">
 							<div className="column">
 								<div className="header-panel__content">
-									<h1 className="panel__heading"><strong>{project.node.childMarkdownRemark.frontmatter.title}</strong></h1>
-									<p className="panel__subheading">{post.frontmatter.title}</p>
+									<h1 className="panel__heading"><strong>{post.frontmatter.title}</strong></h1>
+									<p className="panel__subheading">{project.node.childMarkdownRemark.frontmatter.title}</p>
+									<h2 className="panel__subheading icon-text icon-text--left">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="calendar"><rect width="24" height="24" opacity="0"/><path d="M18 4h-1V3a1 1 0 0 0-2 0v1H9V3a1 1 0 0 0-2 0v1H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zM6 6h1v1a1 1 0 0 0 2 0V6h6v1a1 1 0 0 0 2 0V6h1a1 1 0 0 1 1 1v4H5V7a1 1 0 0 1 1-1zm12 14H6a1 1 0 0 1-1-1v-6h14v6a1 1 0 0 1-1 1z"/><circle cx="8" cy="16" r="1"/><path d="M16 15h-4a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2z"/></g></g></svg>{dateFns.format(new Date(post.frontmatter.date), 'dddd, MMMM D, YYYY')}</h2>
 								</div>
 							</div>
 						</div>
@@ -31,7 +33,6 @@ export default function Template({ data }) {
 				<div className="blog__container">
 					<div className="row">
 						<div className="column column--md-8">
-							<h2 className="blog__date">{dateFns.format(new Date(post.frontmatter.date), 'dddd, MMMM D, YYYY')}</h2>
 							<div className="blog__rte" dangerouslySetInnerHTML={{__html: post.html}} />
 						</div>
 					</div>
