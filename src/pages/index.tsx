0ed767dc09e543633bd83e38835db03dbb98ec78
Mark <mjimenez@habaneroconsulting.com>
2019-03-24 17:54:21 -0400

    Updated styling on posts page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,10 +84,9 @@ const IndexPage = () => (
 											</div>
 											<div className="panel__content column column--md-6">
 												<div className="panel__header">
-													<h2 className="panel__heading">{project.node.childMarkdownRemark.frontmatter.title}</h2>
+													<h2 className="panel__heading">{project.node.childMarkdownRemark.frontmatter.title} {(project.node.childMarkdownRemark.frontmatter.workInProgress ? <span className='pill pill--yellow'>WIP</span> : '')}</h2>
 													<p className="panel__client">
 														{project.node.childMarkdownRemark.frontmatter.client}
-														{(project.node.childMarkdownRemark.frontmatter.workInProgress ? <span className='pill pill--yellow'>WIP</span> : '')}
 													</p>
 												</div>
 												<div className="panel__short-description" dangerouslySetInnerHTML={{__html: project.node.childMarkdownRemark.excerpt}}/>
@@ -132,9 +131,9 @@ const IndexPage = () => (
 										<div className="row">
 											<div className="panel__content column column--md-7">
 												<div className="panel__header">
-													<h2 className="panel__heading">{frontmatter.title}</h2>
+													<h2 className="panel__heading">{frontmatter.title} {(project.node.childMarkdownRemark.frontmatter.workInProgress ? <span className='pill pill--yellow'>WIP</span> : '')}</h2>
 												</div>
-												<div className="panel__short-description panel__short-description--light" dangerouslySetInnerHTML={{__html: project.node.childMarkdownRemark.excerpt}}/>
+												<div className="panel__short-description" dangerouslySetInnerHTML={{__html: project.node.childMarkdownRemark.excerpt}}/>
 												{/* <div className="panel__footer">
 													<a href={project.node.childMarkdownRemark.frontmatter.website.websiteLink} className="panel__link panel__link--icon">
 													<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="globe"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M22 12A10 10 0 0 0 12 2a10 10 0 0 0 0 20 10 10 0 0 0 10-10zm-2.07-1H17a12.91 12.91 0 0 0-2.33-6.54A8 8 0 0 1 19.93 11zM9.08 13H15a11.44 11.44 0 0 1-3 6.61A11 11 0 0 1 9.08 13zm0-2A11.4 11.4 0 0 1 12 4.4a11.19 11.19 0 0 1 3 6.6zm.36-6.57A13.18 13.18 0 0 0 7.07 11h-3a8 8 0 0 1 5.37-6.57zM4.07 13h3a12.86 12.86 0 0 0 2.35 6.56A8 8 0 0 1 4.07 13zm10.55 6.55A13.14 13.14 0 0 0 17 13h2.95a8 8 0 0 1-5.33 6.55z"/></g></g></svg>
