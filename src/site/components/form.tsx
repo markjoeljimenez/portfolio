23de985e440c3118410eb88ea22eb472597b864b
Mark <mjimenez@habaneroconsulting.com>
2019-10-23 17:07:47 -0400

    Fix form

--- src/site/components/form.tsx
+++ src/site/components/form.tsx
@@ -7,18 +7,22 @@ export default class Form extends React.Component {
 
 	public render() {
 		return (
-			<form className="form" name="contact" method="POST" data-netlify="true">
+			<form className="form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+				<input type="hidden" name="form-name" value="contact" hidden />
+				<label className="form__label" htmlFor="bot-field" hidden>Don’t fill this out if you're human:</label>
+				<input type="hidden" name="bot-field" id="bot-field" hidden/>
+
 				<div className="form__group">
-					<label className="u-hidden">Your Name</label>
-					<input className="form__input input" type="text" name="name" placeholder="Your name" required/>
+					<label className="u-hidden" htmlFor="name">Your Name</label>
+					<input id="name" className="form__input input" type="text" name="name" placeholder="Name" required/>
 				</div>
 				<div className="form__group">
-					<label className="u-hidden">Your Email</label>
-					<input className="form__input input" type="email" name="email" placeholder="Your email" required/>
+					<label className="u-hidden" htmlFor="email">Your Email</label>
+					<input id="email" className="form__input input" type="email" name="email" placeholder="Email" required/>
 				</div>
 				<div className="form__group">
-					<label className="u-hidden">Message</label>
-					<textarea className="textarea" name="message" placeholder="Message" required></textarea>
+					<label className="u-hidden" htmlFor="message">Message</label>
+					<textarea id="message" className="textarea" name="message" placeholder="Message" required></textarea>
 				</div>
 				<div className="form__group">
 					<button className="button button--dark" type="submit">Send</button>
