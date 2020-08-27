fc959937b62838aa2402cd4405df3b76c5337ecd
markjoeljimenez <markjoeljimenez@gmail.com>
2020-08-27 18:13:21 -0400

    Add Twitch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,206 +1,212 @@
-require("dotenv").config({
-	path: `.env.${process.env.NODE_ENV}`,
-});
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-	siteMetadata: {
-		title: `Mark Joel Jimenez`,
-		menuPages: [
-			{
-				link: `/`,
-				name: `Home`
-			},
-			{
-				link: `/#work`,
-				name: `Work`
-			},
-			{
-				link: `/#projects`,
-				name: `Projects`
-			}
-			// {
-			// 	link: `/resume`,
-			// 	name: `Resume`
-			// }
-		],
-		socialMedia: [
-			{
-				link: `https://github.com/markjoeljimenez`,
-				name: `Github`,
-				icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M12 1A10.89 10.89 0 0 0 1 11.77 10.79 10.79 0 0 0 8.52 22c.55.1.75-.23.75-.52v-1.83c-3.06.65-3.71-1.44-3.71-1.44a2.86 2.86 0 0 0-1.22-1.58c-1-.66.08-.65.08-.65a2.31 2.31 0 0 1 1.68 1.11 2.37 2.37 0 0 0 3.2.89 2.33 2.33 0 0 1 .7-1.44c-2.44-.27-5-1.19-5-5.32a4.15 4.15 0 0 1 1.11-2.91 3.78 3.78 0 0 1 .11-2.84s.93-.29 3 1.1a10.68 10.68 0 0 1 5.5 0c2.1-1.39 3-1.1 3-1.1a3.78 3.78 0 0 1 .11 2.84A4.15 4.15 0 0 1 19 11.2c0 4.14-2.58 5.05-5 5.32a2.5 2.5 0 0 1 .75 2v2.95c0 .35.2.63.75.52A10.8 10.8 0 0 0 23 11.77 10.89 10.89 0 0 0 12 1" data-name="github"/></g></svg>`
-			},
-			{
-				link: `https://www.linkedin.com/in/mark-jimenez/`,
-				name: `LinkedIn`,
-				icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="linkedin"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M15.15 8.4a5.83 5.83 0 0 0-5.85 5.82v5.88a.9.9 0 0 0 .9.9h2.1a.9.9 0 0 0 .9-.9v-5.88a1.94 1.94 0 0 1 2.15-1.93 2 2 0 0 1 1.75 2v5.81a.9.9 0 0 0 .9.9h2.1a.9.9 0 0 0 .9-.9v-5.88a5.83 5.83 0 0 0-5.85-5.82z"/><rect x="3" y="9.3" width="4.5" height="11.7" rx=".9" ry=".9"/><circle cx="5.25" cy="5.25" r="2.25"/></g></g></svg>`
-			},
-			{
-				link: `mailto:markjoeljimenez@gmail.com`,
-				name: `Email`,
-				icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="email"><rect width="24" height="24" opacity="0"/><path d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm0 2l-6.5 4.47a1 1 0 0 1-1 0L5 6z"/></g></g></svg>`
-			},
-			{
-				link: `tel:647-861-0617`,
-				name: `Phone`,
-				icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="phone"><rect width="24" height="24" opacity="0"/><path d="M17.4 22A15.42 15.42 0 0 1 2 6.6 4.6 4.6 0 0 1 6.6 2a3.94 3.94 0 0 1 .77.07 3.79 3.79 0 0 1 .72.18 1 1 0 0 1 .65.75l1.37 6a1 1 0 0 1-.26.92c-.13.14-.14.15-1.37.79a9.91 9.91 0 0 0 4.87 4.89c.65-1.24.66-1.25.8-1.38a1 1 0 0 1 .92-.26l6 1.37a1 1 0 0 1 .72.65 4.34 4.34 0 0 1 .19.73 4.77 4.77 0 0 1 .06.76A4.6 4.6 0 0 1 17.4 22zM6.6 4A2.61 2.61 0 0 0 4 6.6 13.41 13.41 0 0 0 17.4 20a2.61 2.61 0 0 0 2.6-2.6v-.33L15.36 16l-.29.55c-.45.87-.78 1.5-1.62 1.16a11.85 11.85 0 0 1-7.18-7.21c-.36-.78.32-1.14 1.18-1.59L8 8.64 6.93 4z"/></g></g></svg>`
-			}
-		]
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
-				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
-				display: `minimal-ui`,
-				icon: `src/images/logo.svg`, // This path is relative to the root of the site.
-			},
-		},
-		{
-			resolve: "gatsby-plugin-google-tagmanager",
-				options: {
-				id: "GTM-KLG58F5",
-			
-				// Include GTM in development.
-				// Defaults to false meaning GTM will only be loaded in production.
-				includeInDevelopment: false,
-			
-				// datalayer to be set before GTM is loaded
-				// should be an object or a function that is executed in the browser
-				// Defaults to null
-				defaultDataLayer: { platform: "gatsby" },
-			
-				// Specify optional GTM environment details.
-				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-				// dataLayerName: "YOUR_DATA_LAYER_NAME",
-			},
-		},
-		`gatsby-plugin-offline`,
-		{
-			resolve: `gatsby-plugin-sass`,
-			options: {
-				postCssPlugins: [
-					require('postcss-inline-svg')({
-						path: 'src/'
-					})
-				]
-			}
-		},
-		`gatsby-plugin-sharp`,
-		`gatsby-plugin-typescript`,
-		`gatsby-transformer-sharp`,
-		{
-			resolve: `gatsby-source-contentful`,
-			options: {
-				spaceId: `whjch1nhywzb`,
-				accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
-			},
-		},
-		{
-			resolve: `gatsby-source-contentful`,
-			options: {
-				spaceId: `whjch1nhywzb`,
-				accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-				host: `preview.contentful.com`
-			},
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				path: `${__dirname}/src/images/assets`,
-				name: `assets`
-			},
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				path: `${__dirname}/src/images/`,
-				name: `images`
-			},
-		},
-		{
-			resolve: `gatsby-transformer-remark`,
-			options: {
-				"excerpt_separator": `<!-- end -->`,
-				plugins: [
-					{
-						resolve: `gatsby-remark-relative-images`,
-					},
-					{
-						resolve: `gatsby-remark-images`,
-						options: {
-							maxWidth: 720,
-							showCaptions: true
-						}
-					},
-					{
-						resolve: `gatsby-remark-prismjs`,
-						options: {
-						  // Class prefix for <pre> tags containing syntax highlighting;
-						  // defaults to 'language-' (eg <pre class="language-js">).
-						  // If your site loads Prism into the browser at runtime,
-						  // (eg for use with libraries like react-live),
-						  // you may use this to prevent Prism from re-processing syntax.
-						  // This is an uncommon use-case though;
-						  // If you're unsure, it's best to use the default value.
-						  classPrefix: "language-",
-						  // This is used to allow setting a language for inline code
-						  // (i.e. single backticks) by creating a separator.
-						  // This separator is a string and will do no white-space
-						  // stripping.
-						  // A suggested value for English speakers is the non-ascii
-						  // character '›'.
-						  inlineCodeMarker: null,
-						  // This lets you set up language aliases.  For example,
-						  // setting this to '{ sh: "bash" }' will let you use
-						  // the language "sh" which will highlight using the
-						  // bash highlighter.
-						  aliases: {},
-						  // This toggles the display of line numbers globally alongside the code.
-						  // To use it, add the following line in src/layouts/index.js
-						  // right after importing the prism color scheme:
-						  //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
-						  // Defaults to false.
-						  // If you wish to only show line numbers on certain code blocks,
-						  // leave false and use the {numberLines: true} syntax below
-						  showLineNumbers: true,
-						  // If setting this to true, the parser won't handle and highlight inline
-						  // code used in markdown i.e. single backtick code like `this`.
-						  noInlineHighlight: false,
-						  // This adds a new language definition to Prism or extend an already
-						  // existing language definition. More details on this option can be
-						  // found under the header "Add new language definition or extend an
-						  // existing language" below.
-						  languageExtensions: [
-							{
-							  language: "superscript",
-							  extend: "javascript",
-							  definition: {
-								superscript_types: /(SuperType)/,
-							  },
-							  insertBefore: {
-								function: {
-								  superscript_keywords: /(superif|superelse)/,
-								},
-							  },
-							},
-						  ],
-						  // Customize the prompt used in shell output
-						  // Values below are default
-						  prompt: {
-							user: "root",
-							host: "localhost",
-							global: false,
-						  },
-						},
-					}
-				]
-			}
-		}
-	],
+  siteMetadata: {
+    title: `Mark Joel Jimenez`,
+    menuPages: [
+      {
+        link: `/`,
+        name: `Home`,
+      },
+      {
+        link: `/#work`,
+        name: `Work`,
+      },
+      {
+        link: `/#projects`,
+        name: `Projects`,
+      },
+      // {
+      // 	link: `/resume`,
+      // 	name: `Resume`
+      // }
+    ],
+    socialMedia: [
+      {
+        link: `https://github.com/markjoeljimenez`,
+        name: `Github`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M12 1A10.89 10.89 0 0 0 1 11.77 10.79 10.79 0 0 0 8.52 22c.55.1.75-.23.75-.52v-1.83c-3.06.65-3.71-1.44-3.71-1.44a2.86 2.86 0 0 0-1.22-1.58c-1-.66.08-.65.08-.65a2.31 2.31 0 0 1 1.68 1.11 2.37 2.37 0 0 0 3.2.89 2.33 2.33 0 0 1 .7-1.44c-2.44-.27-5-1.19-5-5.32a4.15 4.15 0 0 1 1.11-2.91 3.78 3.78 0 0 1 .11-2.84s.93-.29 3 1.1a10.68 10.68 0 0 1 5.5 0c2.1-1.39 3-1.1 3-1.1a3.78 3.78 0 0 1 .11 2.84A4.15 4.15 0 0 1 19 11.2c0 4.14-2.58 5.05-5 5.32a2.5 2.5 0 0 1 .75 2v2.95c0 .35.2.63.75.52A10.8 10.8 0 0 0 23 11.77 10.89 10.89 0 0 0 12 1" data-name="github"/></g></svg>`,
+      },
+      {
+        link: `https://www.linkedin.com/in/mark-jimenez/`,
+        name: `LinkedIn`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="linkedin"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M15.15 8.4a5.83 5.83 0 0 0-5.85 5.82v5.88a.9.9 0 0 0 .9.9h2.1a.9.9 0 0 0 .9-.9v-5.88a1.94 1.94 0 0 1 2.15-1.93 2 2 0 0 1 1.75 2v5.81a.9.9 0 0 0 .9.9h2.1a.9.9 0 0 0 .9-.9v-5.88a5.83 5.83 0 0 0-5.85-5.82z"/><rect x="3" y="9.3" width="4.5" height="11.7" rx=".9" ry=".9"/><circle cx="5.25" cy="5.25" r="2.25"/></g></g></svg>`,
+      },
+      {
+        link: `mailto:markjoeljimenez@gmail.com`,
+        name: `Email`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="email"><rect width="24" height="24" opacity="0"/><path d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm0 2l-6.5 4.47a1 1 0 0 1-1 0L5 6z"/></g></g></svg>`,
+      },
+      {
+        link: `tel:647-861-0617`,
+        name: `Phone`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="phone"><rect width="24" height="24" opacity="0"/><path d="M17.4 22A15.42 15.42 0 0 1 2 6.6 4.6 4.6 0 0 1 6.6 2a3.94 3.94 0 0 1 .77.07 3.79 3.79 0 0 1 .72.18 1 1 0 0 1 .65.75l1.37 6a1 1 0 0 1-.26.92c-.13.14-.14.15-1.37.79a9.91 9.91 0 0 0 4.87 4.89c.65-1.24.66-1.25.8-1.38a1 1 0 0 1 .92-.26l6 1.37a1 1 0 0 1 .72.65 4.34 4.34 0 0 1 .19.73 4.77 4.77 0 0 1 .06.76A4.6 4.6 0 0 1 17.4 22zM6.6 4A2.61 2.61 0 0 0 4 6.6 13.41 13.41 0 0 0 17.4 20a2.61 2.61 0 0 0 2.6-2.6v-.33L15.36 16l-.29.55c-.45.87-.78 1.5-1.62 1.16a11.85 11.85 0 0 1-7.18-7.21c-.36-.78.32-1.14 1.18-1.59L8 8.64 6.93 4z"/></g></g></svg>`,
+      },
+      {
+        link: 'https://www.twitch.tv/bweebztv',
+        name: 'Twitch',
+        icon:
+          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#fff"><path d="M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7"/></svg>',
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-KLG58F5',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require('postcss-inline-svg')({
+            path: 'src/',
+          }),
+        ],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `whjch1nhywzb`,
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `whjch1nhywzb`,
+        accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+        host: `preview.contentful.com`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/`,
+        name: `images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        excerpt_separator: `<!-- end -->`,
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 720,
+              showCaptions: true,
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              // Class prefix for <pre> tags containing syntax highlighting;
+              // defaults to 'language-' (eg <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (eg for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: 'language-',
+              // This is used to allow setting a language for inline code
+              // (i.e. single backticks) by creating a separator.
+              // This separator is a string and will do no white-space
+              // stripping.
+              // A suggested value for English speakers is the non-ascii
+              // character '›'.
+              inlineCodeMarker: null,
+              // This lets you set up language aliases.  For example,
+              // setting this to '{ sh: "bash" }' will let you use
+              // the language "sh" which will highlight using the
+              // bash highlighter.
+              aliases: {},
+              // This toggles the display of line numbers globally alongside the code.
+              // To use it, add the following line in src/layouts/index.js
+              // right after importing the prism color scheme:
+              //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
+              // Defaults to false.
+              // If you wish to only show line numbers on certain code blocks,
+              // leave false and use the {numberLines: true} syntax below
+              showLineNumbers: true,
+              // If setting this to true, the parser won't handle and highlight inline
+              // code used in markdown i.e. single backtick code like `this`.
+              noInlineHighlight: false,
+              // This adds a new language definition to Prism or extend an already
+              // existing language definition. More details on this option can be
+              // found under the header "Add new language definition or extend an
+              // existing language" below.
+              languageExtensions: [
+                {
+                  language: 'superscript',
+                  extend: 'javascript',
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              // Customize the prompt used in shell output
+              // Values below are default
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: false,
+              },
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
--- src/site/components/panels/contact-panel.tsx
+++ src/site/components/panels/contact-panel.tsx
@@ -1,64 +1,80 @@
-import React from 'react';
-import Modal from '../modal';
-import Form from '../form';
+import React from 'react'
+import Modal from '../modal'
+import Form from '../form'
 
 interface IContactPanelState {
-	hidden: boolean;
+  hidden: boolean
 }
 
-export default class ContactPanel extends React.Component<{}, IContactPanelState> {
-	private modalRef;
+export default class ContactPanel extends React.Component<
+  {},
+  IContactPanelState
+> {
+  private modalRef
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props)
 
-		this.modalRef = React.createRef();
+    this.modalRef = React.createRef()
 
-		this.state = {
-			hidden: true
-		}
-	}
+    this.state = {
+      hidden: true,
+    }
+  }
 
-	private toggleModalVisibility() {
-		this.setState({
-			hidden: !this.state.hidden
-		}, this.togglePageOverflow)
-	}
+  private toggleModalVisibility() {
+    this.setState(
+      {
+        hidden: !this.state.hidden,
+      },
+      this.togglePageOverflow
+    )
+  }
 
-	private togglePageOverflow() {
-		const body = this.modalRef.current.closest('html');
+  private togglePageOverflow() {
+    const body = this.modalRef.current.closest('html')
 
-		if (body instanceof HTMLElement) {
-			if (!this.state.hidden) {
-				body.classList.add('overflow-hidden');
-			} else {
-				body.classList.remove('overflow-hidden');
-			}
-		}
-	}
+    if (body instanceof HTMLElement) {
+      if (!this.state.hidden) {
+        body.classList.add('overflow-hidden')
+      } else {
+        body.classList.remove('overflow-hidden')
+      }
+    }
+  }
 
-	public render() {
-		return (
-			<>
-				<div className="hightlight-panel panel panel--dark panel--has-background-color panel--small-spacing panel--text-align-center">
-					<div className="panel__container">
-						<div className="row">
-							<div className="column">
-								<h2 className="panel__heading"><strong>Interested in working together?</strong></h2>
-								<p className="panel__subheading panel__subheading--large text text--light">Send me a message and let's get started!</p>
-								<button className="highlight-panel__button button" onClick={this.toggleModalVisibility.bind(this)}>Contact me</button>
-							</div>
-						</div>
-					</div>
-				</div>
+  public render() {
+    return (
+      <>
+        <div className="hightlight-panel panel panel--dark panel--has-background-color panel--small-spacing panel--text-align-center">
+          <div className="panel__container">
+            <div className="row">
+              <div className="column">
+                <h2 className="panel__heading">
+                  <strong>Interested in working together?</strong>
+                </h2>
+                <p className="panel__subheading panel__subheading--large text text--light">
+                  Let's get started!
+                </p>
+                <button
+                  className="highlight-panel__button button"
+                  onClick={this.toggleModalVisibility.bind(this)}
+                >
+                  Contact me
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
 
-				<Modal
-					hidden={this.state.hidden}
-					toggleModalVisibility={this.toggleModalVisibility.bind(this)}
-					modalRef={this.modalRef}>
-					<Form />
-				</Modal>
-			</>
-		)
-	}
+        <Modal
+          hidden={this.state.hidden}
+          toggleModalVisibility={this.toggleModalVisibility.bind(this)}
+          modalRef={this.modalRef}
+        >
+          <Form />
+        </Modal>
+      </>
+    )
+  }
 }
