8bdd92cb96355f09b2e06c3ff2cd3ba28d1c8fe9
Mark Jimenez <markjoeljimenez@gmail.com>
2022-01-08 23:30:50 -0500

    Potential build fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,44 @@
-const path = require('path');
+const path = require('path')
 
-exports.createPages = ({actions, graphql}) => {
-	const {createPage} = actions;
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
 
-	return graphql(`{
-		allContentfulProject {
-			edges {
-				node {
-					path
-				}
-			}
-		}
-		allContentfulPost {
-			edges {
-				node {
-					path
-				}
-			}
-		}
-	}`)
-	.then(res => {
-		if(res.errors) {
-			return Promise.reject(res.errors);
-		}
+  return graphql(`
+    {
+      allContentfulProject {
+        edges {
+          node {
+            path
+          }
+        }
+      }
+      allContentfulPost {
+        edges {
+          node {
+            path
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
 
-		// Create project pages
-		res.data.allContentfulProject.edges.forEach(({node}) => {
-			createPage({
-				path: node.path,
-				component: path.resolve(`src/site/templates/project.tsx`)
-			})
-		});
+    // Create project pages
+    res.data.allContentfulProject.edges.forEach(({ node }) => {
+      createPage({
+        path: '/',
+        component: path.resolve(`src/site/templates/project.tsx`),
+      })
+    })
 
-		// Create post pages
-		res.data.allContentfulPost.edges.forEach(({node}) => {
-			createPage({
-				path: node.path,
-				component: path.resolve(`src/site/templates/post.tsx`)
-			})
-		});
-	})
+    // Create post pages
+    res.data.allContentfulPost.edges.forEach(({ node }) => {
+      createPage({
+        path: '/',
+        component: path.resolve(`src/site/templates/post.tsx`),
+      })
+    })
+  })
 }
