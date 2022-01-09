93c6153baccf916cc0c28735760f5ef0690ec744
Mark Jimenez <markjoeljimenez@gmail.com>
2022-01-08 23:36:53 -0500

    Remove Link from projects

--- src/site/components/panels/project-panel.tsx
+++ src/site/components/panels/project-panel.tsx
@@ -51,38 +51,32 @@ export default class ProjectPanel extends React.Component<IProjectPanelProps> {
               <div className="panel__header">
                 <h2 className="panel__heading">
                   {this.props.settings.path &&
-                  this.props.settings.links.websiteLink ? (
-                    this.props.settings.links &&
-                    this.props.settings.featured ? (
-                      <a
-                        href={this.props.settings.links.websiteLink}
-                        target="_blank"
-                        rel="noreffer"
-                      >
-                        {this.props.settings.heading}
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          width="24"
-                          height="24"
+                  this.props.settings.links.websiteLink
+                    ? this.props.settings.links &&
+                      this.props.settings.featured && (
+                        <a
+                          href={this.props.settings.links.websiteLink}
+                          target="_blank"
+                          rel="noreffer"
                         >
-                          <g data-name="Layer 2">
-                            <g data-name="external-link">
-                              <rect width="24" height="24" opacity="0" />
-                              <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
-                              <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
+                          {this.props.settings.heading}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                          >
+                            <g data-name="Layer 2">
+                              <g data-name="external-link">
+                                <rect width="24" height="24" opacity="0" />
+                                <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
+                                <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
+                              </g>
                             </g>
-                          </g>
-                        </svg>
-                      </a>
-                    ) : (
-                      <Link to={this.props.settings.path}>
-                        {this.props.settings.heading}
-                      </Link>
-                    )
-                  ) : (
-                    this.props.settings.heading
-                  )}
+                          </svg>
+                        </a>
+                      )
+                    : this.props.settings.heading}
                   {/* {this.props.settings.workInProgress ? <span className='pill pill--yellow'>WIP</span> : ''} */}
                 </h2>
                 {this.props.settings.client ? (
